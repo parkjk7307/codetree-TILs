a7a6d23b53caf83659c413924aa77195d4a45b3a
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-04-20 14:44:06 +0900

    Title: 4번의 크기 비교, Time: 224ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240420/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,28 @@
+a = int(input())
+b = input().split()
+
+q = int(b[0])
+w = int(b[1])
+e = int(b[2])
+r = int(b[3])
+
+
+if a > q :
+    print(1)
+else :
+    print(0)
+
+if a > w :
+    print(1)
+else :
+    print(0)
+
+if a > e :
+    print(1)
+else :
+    print(0)
+
+if a > r :
+    print(1)
+else :
+    print(0)
