2496a04b8f8da004dd7d69a251769d2a28f90994
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-04-20 15:21:39 +0900

    Title: 특정 조건 세 정수 비교, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240420/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,15 @@
+a = input().split()
+
+x = int(a[0])
+y = int(a[1])
+z = int(a[2])
+
+if x == y or x == z:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
+if x == y and y == z:
+    print(1)
+else:
+    print(0)
