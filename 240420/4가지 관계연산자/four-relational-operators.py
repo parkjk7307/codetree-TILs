11ce13a09d5a0f732365f4cd0a9af37010ecfcfc
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-04-20 14:41:08 +0900

    Title: 4가지 관계연산자, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240420/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,23 @@
+a = input().split()
+x = int(a[0])
+y = int(a[1])
+
+if x >= y :
+    print(1)
+else :
+    print(0)
+
+if x > y :
+    print(1)
+else :
+    print(0)
+
+if y >= x :
+    print(1)
+else :
+    print(0)
+
+if y > x :
+    print(1)
+else :
+    print(0)
