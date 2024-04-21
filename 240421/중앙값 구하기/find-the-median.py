1ee8c9825d059fa2a68c7012cb3097f5eaa53210
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-04-21 17:49:25 +0900

    Title: 중앙값 구하기, Time: 257ms, Memory: 24MB, Status: Passed - CodeTree

--- 240421/중앙값 구하기/find-the-median.py
+++ 240421/중앙값 구하기/find-the-median.py
@@ -4,9 +4,20 @@ x = int(a[0])
 y = int(a[1])
 z = int(a[2])
 
-if x > y and x < z :
-    print(x)
-elif y > x and y < z :
-    print(y)
-elif z > x and z < y :
-    print(z)
+if x > y and x > z :
+    if y > z :
+        print(y)
+    elif y < z:
+        print(z)
+
+elif y > x and y > z :
+    if x > z:
+        print(x)
+    elif x < z:
+        print(z)
+
+elif z > x and z > y :
+    if x > y:
+        print(x)
+    elif x < y:
+        print(y)
