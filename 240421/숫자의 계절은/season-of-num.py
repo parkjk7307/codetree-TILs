5d64d5c4155b3887e1da0e1ba698ef7b1c255133
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-04-21 15:06:53 +0900

    Title: 숫자의 계절은, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240421/숫자의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+a = int(input())
+
+if a >= 3 and a <= 5:
+    print("Spring")
+elif a >= 6 and a <= 8:
+    print("Summer")
+elif a >= 9 and a <= 11:
+    print("Fall")
+elif a >= 12 and a <= 2:
+    print("Winter")
