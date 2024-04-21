abf35be1214589f4512ebb2b4e413c9228cd1a2c
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-04-21 15:24:33 +0900

    Title: 두 사람, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240421/두 사람/two-person.py
@@ -0,0 +1,13 @@
+a = input().split()
+b = input().split()
+
+x = int(a[0])
+y = a[1]
+
+o = int(b[0])
+p = b[1]
+
+if ((x >= 19 or o >= 19) and y == "M") or ((x >= 19 or o >= 19) and p == "M"):
+    print("1")
+else:
+    print("0")
