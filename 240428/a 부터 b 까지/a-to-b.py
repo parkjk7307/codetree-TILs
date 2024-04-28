70039bceea8ba685585f1389527f6306cb2444ed
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-04-28 17:54:27 +0900

    Title: a 부터 b 까지, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240428/a 부터 b 까지/a-to-b.py
+++ 240428/a 부터 b 까지/a-to-b.py
@@ -3,11 +3,10 @@ x = input().split()
 a = int(x[0])
 b = int(x[1])
 
-for i in range (a, b+1):
-    if b >= i:
-        if i % 2 == 1:
-            print(i, end = " ")
-            i *= 2
-        elif i % 2 == 0:
-            print(i, end = " ")
-            i += 3
+while b >= a:
+    if a % 2 == 1:
+        print(a, end = " ")
+        a *= 2
+    elif a % 2 == 0:
+        print(a, end = " ")
+        a += 3
