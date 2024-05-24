16b74ca6726e568dce75928f31cf2ed802f46abe
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-05-24 15:16:05 +0900

    Title: 출석 부르기 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240524/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,17 @@
+while True:
+    a = int(input())
+    if a == 1:
+        print("John")
+        continue
+    elif a == 2:
+        print("Tom")
+        continue
+    elif a == 3:
+        print("Paul")
+        continue
+    elif a == 4:
+        print("Sam")
+        continue
+    else:
+        print("Vacancy")
+        break
