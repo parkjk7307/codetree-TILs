c055f4d2e4a1b1dc87f40a11903b8cd1aaa3f5e5
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-05-24 16:37:03 +0900

    Title: 20대의 평균나이, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240524/20대의 평균나이/average-age-of-20.py
+++ 240524/20대의 평균나이/average-age-of-20.py
@@ -1,16 +1,12 @@
 sum_val = 0
 cnt = 0
 avg_val = 0
-x = 0
 while True:
     x = int(input())
-    if ((x % 20) >= 0 and (x % 20) < 10):
+    if 19 < x < 30:
         sum_val += x
         cnt += 1
-        continue
     else:
         break
 
-avg_val = sum_val / cnt
-
-print(f"{avg_val:.2f}")
+print(f"{sum_val/cnt:.2f}")
