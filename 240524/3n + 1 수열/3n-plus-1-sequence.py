90cde4d6a9a59a0eb712c89644f9cd4e92bd277a
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-05-24 16:57:28 +0900

    Title: 3n + 1 수열, Time: 226ms, Memory: 24MB, Status: Passed - CodeTree

--- 240524/3n + 1 수열/3n-plus-1-sequence.py
+++ 240524/3n + 1 수열/3n-plus-1-sequence.py
@@ -2,14 +2,12 @@ a = int(input())
 cnt = 0
 
 while True:
-    if a % 2 == 0:
+    if a == 1:
+        break
+    elif a % 2 == 0:
         a = a // 2
         cnt += 1
     elif a % 2 == 1:
         a = a * 3 + 1
         cnt += 1
-    
-    if a == 1:
-        break
-
 print(cnt)
