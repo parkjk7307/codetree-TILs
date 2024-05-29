cdd5ca344696af2e800329709415f5137a134414
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-05-29 14:08:39 +0900

    Title: 별 그리기, Time: 243ms, Memory: 27MB, Status: Passed - CodeTree

--- 240529/별 그리기/star-drawing.py
+++ 240529/별 그리기/star-drawing.py
@@ -9,6 +9,6 @@ for i in range(n):
 for a in range(n-1):
     for b in range(a+1):
         print(" ",end = "")
-    for c in range((2*n)-(2*a)-n):
+    for c in range((2*n)-(2*a)-3):
         print("*", end="")
     print()
