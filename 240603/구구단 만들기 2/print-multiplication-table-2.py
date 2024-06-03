a41831d446ca0accba805a7a39a1854e3ab99dcb
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-06-03 21:59:10 +0900

    Title: 구구단 만들기 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240603/구구단 만들기 2/print-multiplication-table-2.py
+++ 240603/구구단 만들기 2/print-multiplication-table-2.py
@@ -4,14 +4,13 @@ a = int(n[0])
 b = int(n[1])
 c = (b-a)//2+2
 d = 1
-cnt = b
 val = 2*d
 
 
 for i in range(4):
-    for j in range(c):
+    for j in range(b-a+1):
         print(f"{b-j} * {val * d} = {(b-j)*(val*d)} ", end = "")
-        if j < c-1:
+        if j < b-a:
             print("/ ",end = "")
     d += 1
     print()
