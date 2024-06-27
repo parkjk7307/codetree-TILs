9129a00b7ada7b95dfc2228ea577d8d8928663bc
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-06-27 14:51:48 +0900

    Title: 학점 계산기, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240627/학점 계산기/credit-calculator.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = list(map(float,input().split()))
+
+z = sum(y)/x
+
+print(f"{z:.1f}")
+
+if z > 4.0:
+    print("Perfect")
+elif z > 3.0:
+    print("Good")
+else :
+    print("Poor")
