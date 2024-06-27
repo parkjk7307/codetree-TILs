350768913f6b5163339f45917c8b53f2f1011e8f
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-06-27 16:00:59 +0900

    Title: 특정 숫자 도달하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240627/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,14 @@
+x = list(map(int,input().split()))
+
+n = len(x)
+sum_val = 0
+count = 0
+
+for i in range(n):
+    if x[i] >= 250:
+        break
+    else:
+        sum_val += x[i]
+        count +=1
+
+print(sum_val,sum_val/count, end = " ")
