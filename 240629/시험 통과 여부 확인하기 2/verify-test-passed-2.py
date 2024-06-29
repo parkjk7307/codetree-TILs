e0c6e62138f1ada5294d65d1c9773ac254805ff6
parkjk7307 <76420796+parkjk7307@users.noreply.github.com>
2024-06-29 22:35:14 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 182ms, Memory: 24MB, Status: Passed - CodeTree

--- 240629/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 240629/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -1,17 +1,18 @@
 x = int(input())
-y = list(map(int,input().split()))
 
-sum_val = 0  
+
 cnt = 0
 
 for i in range(x):
+    y = list(map(int,input().split()))
+    sum_val = 0 
     for j in y:
         sum_val += j
+    
     c = sum_val/4
     if c >= 60:
         print("pass")
         cnt += 1
-        sum_val = 0
     elif c < 60:
         print("fail")
 
